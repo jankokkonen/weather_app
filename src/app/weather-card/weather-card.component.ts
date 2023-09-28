3431aa363b554103bf7224e244b61b4b982a4cb5
jankokkonen <jan.kokkonen7@gmail.com>
2023-09-28 15:04:31 +0300

    Create weather service connection and weather-card component

---
+++ src/app/weather-card/weather-card.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../Services/weather.service';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: ['./weather-card.component.css']
+})
+export class WeatherCardComponent implements OnInit {
+  weatherData: any[] = [];
+
+  constructor (private WeatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.loadWeatherData();
+  }
+
+  loadWeatherData() {
+    this.WeatherService.getWeatherData().subscribe(data => {
+      this.weatherData = data;
+      console.log(this.weatherData)
+    })
+  }
+}
