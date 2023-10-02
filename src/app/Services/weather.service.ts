f3dd2ff10b33ea0bce754bb6b083367a45c55637
jankokkonen <jan.kokkonen7@gmail.com>
2023-10-02 14:48:31 +0300

    Create environment.ts and fix weather.service.ts

--- src/app/Services/weather.service.ts
+++ src/app/Services/weather.service.ts
@@ -1,29 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable} from 'rxjs';
-import { catchError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from 'src/environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-    private readonly url = 'http://api.weatherapi.com/v1';
-    private readonly apiKey = 'c7539f62442342ba9b4115553232809';
- 
 
   constructor(private http: HttpClient) { }
 
-  getWeatherData(): Observable<any> {
-    const headers = new HttpHeaders ({
-      'API-Key': this.apiKey,
-      'Host': 'http://api.weatherapi.com/v1'
-    });
-
-    return this.http.get(this.url, { headers }).pipe(
-      catchError((error: any) => {
-        console.error('Virhe:', error);
-        throw new Error('Jotain meni pieleen. Yritä uudelleen myöhemmin.');
-      })
-    );
+  getWeatherData(cityName: string) {
+    this.http.get(environment.weatherApiBaseUrl, {
+      headers: new HttpHeaders()
+      .set (
+        environment.XRapidAPIHostHeaderName, 
+        environment.XRapidAPIHostHeaderValue
+      )
+      .set (
+        environment.XRapidAPIKeyHeaderName, 
+        environment.XRapidAPIKeyHeaderValue
+      ),
+      params: new HttpParams()
+      .set (
+        'q', cityName
+      )
+      .set (
+        'units', 'metric'
+      )
+      .set (
+        'mode', 'json'
+      )
+    })
   }
 }
---
+++ src/environments/environments.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIKeyHeaderValue: '9606db2919mshe19826c987d6c81p1fb9c6jsn56cd5bd10d1a'
+}
