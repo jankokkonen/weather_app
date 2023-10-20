a01da3b2035f6ef148946b5a9a71787e2e1e2ff5
jankokkonen <jan.kokkonen7@gmail.com>
2023-10-20 15:30:10 +0300

    Auto-generated commit [ci skip]

--- src/environments/environments.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: false,
-    currentWeatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json?',
-    forecastWeatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/forecast.json?',
-    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
-    XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
-    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
-    XRapidAPIKeyHeaderValue: '9606db2919mshe19826c987d6c81p1fb9c6jsn56cd5bd10d1a'
-}
